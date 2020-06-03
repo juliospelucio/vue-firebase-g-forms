9193369ab464626dcd860a059751ccfaee4a9fee
foxtsune <juliopeluciofox@hotmail.com>
2020-06-03 07:17:55 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app dark>
+      <div class="d-flex align-center ml-5">
+        <span>Form WebApp</span>
+      </div>
+    </v-app-bar>
+
+    <v-content>
+      <Form />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Form from "./components/Form";
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Form
+  },
+  data: () => ({
+    //
+  })
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,122 @@
+<template>
+<v-container>
+    <v-layout text-center wrap>
+      <v-flex xs12>
+
+        <v-form
+          ref="form"
+          class="mt-4"
+          style="text-align:center;"
+          action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSc7OgfVBSVJNJQMzQ8LH1xnUgi0tQvudopdyOB1pQraENV_ug/formResponse"
+          target="_blank"
+          method="POST"
+          id="mG61Hd" >
+          
+          <v-text-field
+            name="entry.293187218"
+            id="name"
+            prepend-inner-icon="mdi-account"
+            v-model="name"
+            label="Name"
+            :rules="nameRules"
+            outlined
+            shaped
+            hint="Enter Your Name"
+            clearable
+            required
+            color="orange"
+            type="text"
+          ></v-text-field>
+
+          <v-text-field
+            name="entry.621278995"
+            prepend-inner-icon="mdi-email"
+            id="email"
+            v-model="email"
+            :rules="emailRules"      
+            label="Email"
+            hint="For example, abc@gmail.com"
+            clearable
+            required
+            color="orange"
+            type="text"
+            outlined
+            shaped
+          ></v-text-field>
+
+          <v-text-field
+            name="entry.936819236"
+            prepend-inner-icon="mdi-phone"
+            id="phone"
+            :v-model="{phone}"
+            :rules="phoneRules"            
+            label="Phone Number"
+            hint="For example, 123 456 789"
+            clearable
+            required
+            color="orange"
+            prefix="+251 "
+            outlined
+            shaped
+          ></v-text-field>
+
+          <v-textarea
+            name="entry.333762946"
+            id="message"
+            v-model="message"            
+            label="Your Message"
+            hint="Write Down Your Thoughts and we'll get to you"
+            outlined
+            shaped
+            clearable
+            auto-grow
+            counter="400"
+            color="orange"
+            type="text"
+          ></v-textarea>
+
+          <v-btn
+            class="mr-4 elevation-5"
+            type="submit"
+            @click="validate"
+          >Submit</v-btn>
+
+          <v-btn class="mr-4 elevation-5" @click="reset">Reset Form</v-btn>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+
+  name: "Form",
+
+  data: () => ({
+
+    name: "",
+    email: "",
+    message: "",
+    phone: "",
+
+    nameRules: [v => !!v || "Name is required"],
+    phoneRules: [v => !!v || "Number is required"],
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+    ]
+    
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    reset() {
+      this.$refs.form.reset();
+    }
+  }
+};
+</script>
